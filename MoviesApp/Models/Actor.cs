810a3a9bdba6e8a8941c4e482d3fa9df446fc54b
QuackDuster <jjimenez3@ucol.mx>
2022-05-24 11:42:36 -0500

    Initial commit - Uploading the base of the project
    Base of the movies application

---
+++ MoviesApp/Models/Actor.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoviesApp.Models
+{
+    [Table("Actores")]
+    public class Actor
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string MothersLastN { get; set; }
+        public string MoviesPart { get; set; }
+
+    }
+}
---
+++ MoviesApp/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoviesApp.Models
+{
+    [Table("Movies")]
+    public class Movie
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Coverpage { get; set; }
+        public string Title { get; set; }
+        public string Synopsis { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Producer { get; set; }
+    }
+}
